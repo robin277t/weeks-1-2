3f2b2074d8569675414df1975a66aa9168d86bc2
Robin <Robin@MASTUDENT-Robin-C17GMAY1Q6L4.local>
2022-10-11 14:43:46 +0100

    first rpsec tests FAIL TO CHECK WITH COACHES

---
+++ lib/counter.rb
@@ -0,0 +1,13 @@
+class Counter
+    def initialize
+      @count = "0"
+    end
+  
+    def add(num)
+      @count1 = @count + num
+    end 
+  
+    def report
+      return "Counted to #{@count1} so far"
+    end
+  end
---
+++ spec/counter_spec.rb
@@ -0,0 +1,39 @@
+require 'counter'
+
+RSpec.describe Counter do
+    it "pass the test?" do
+        counter = Counter.new()
+        counter.add(3)    
+        result = counter.report
+        expect(result).to eq "Counted to 3 so far"
+    end
+end
+
+RSpec.describe Counter do
+    it "pass the test?" do
+        counter = Counter.new()
+        counter.add(3)
+        counter.add(100)    
+        result = counter.report
+        expect(result).to eq "Counted to 103 so far"
+    end
+end
+
+RSpec.describe Counter do
+    it "pass the test?" do
+        counter = Counter.new()
+        counter.add(20)
+        counter.add(5.4)    
+        result = counter.report
+        expect(result).to eq "Counted to 25.4 so far"
+    end
+end
+
+RSpec.describe Counter do
+    it "pass the test?" do
+        counter = Counter.new()
+        counter.add("str")   
+        result = counter.report
+        expect(result).to eq "Counted to 0str"
+    end
+end

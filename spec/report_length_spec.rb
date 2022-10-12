f83bd450bc6134fae6c1de4075ed00bf644983b7
Robin <Robin@MASTUDENT-Robin-C17GMAY1Q6L4.local>
2022-10-12 10:15:51 +0100

    finished tasks again

---
+++ spec/report_length_spec.rb
@@ -0,0 +1,11 @@
+require 'report_length'
+
+RSpec.describe "tests" do
+    it "length test 1" do
+        expect(report_length("Jeff")).to eq "This string was 4 characters long."
+    end
+
+    it "length test 1" do
+        expect(report_length("Geoffrey the hobo")).to eq "This string was 17 characters long."
+    end
+end
